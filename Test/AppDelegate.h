b0cd250a17a04c56914e5c24872f0615d1516f9c
Valentin Alejandro <Alex@Valentins-MacBook-Pro.local>
2015-06-18 11:24:06 -0500

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Valentin Alejandro on 18/06/15.
+//  Copyright (c) 2015 Valentin Alejandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Valentin Alejandro on 18/06/15.
+//  Copyright (c) 2015 Valentin Alejandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
